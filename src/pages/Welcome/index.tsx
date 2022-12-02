4c6ff4beb1137ebde100ceebb16765fdf299d835
Lucas Fischer <fischer-lucas@hotmail.com>
2022-12-02 00:05:40 +0000

    first commit

---
+++ src/pages/Welcome/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./styles";
+export interface WelcomeProps{
+
+};
+export const Welcome = ({
+
+}: WelcomeProps): JSX.Element => {
+    return (
+        <Container></Container>
+    )
+};
+
---
+++ src/routes/pages/authPages.ts
@@ -0,0 +1,16 @@
+import {
+  Welcome,
+  Login
+} from "../../pages"
+const authPages = [
+  {
+    title: "Welcome",
+    component: Welcome
+  },
+  {
+    title: "Login",
+    component: Login
+  },
+]
+
+export default authPages;
